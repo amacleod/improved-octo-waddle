ca159ab3cd2464c00e6eb03ebbc79185c295240c
Allister MacLeod <allister.macleod@gmail.com>
2017-05-07 21:44:15 -0400

    Hooking up javascript to calculate discounted rate.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 "use strict";
 
+function calculateRates() {
+    console.log("calculating rates");
+    var baseRate = $("#base-rate").val();
+    var discount = $("#discount-percent").val() * 0.01;
+    console.log("base rate: " + baseRate + ", discount: " + discount + "%");
+    var discountedBase = baseRate * (1.0 - discount);
+    populateDiscountTwoDayRate(discountedBase);
+}
 
+function populateDiscountTwoDayRate(rate) {
+    $("#discounted-base").text(rate);
+}
